ff5ce96ed7bb1e62eb53e03e4ab750dfb1f9c3c6
Michael Zemsky <zmike808@gmail.com>
2013-11-09 13:50:39 -0500

    VICTORY

--- WorkItProject/WorkIt/src/main/java/com/yhack/WorkIt/MainActivity.java
+++ WorkItProject/WorkIt/src/main/java/com/yhack/WorkIt/MainActivity.java
@@ -206,12 +206,7 @@ public class MainActivity extends ActionBarActivity {
 
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+
     }
 
 }
